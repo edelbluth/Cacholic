a41ed48e2710f5067a69d5faed08d8d378136d05
Jürgen Edelbluth <dev@jued.de>
2013-07-22 21:35:56 +0200

    FastPath -> added fast access to configuration

--- program/Cacholic/src/main/java/de/jued/dev/gc/cacholic/db/FastPath.java
+++ program/Cacholic/src/main/java/de/jued/dev/gc/cacholic/db/FastPath.java
@@ -2,8 +2,13 @@ package de.jued.dev.gc.cacholic.db;
 
 import de.jued.dev.gc.cacholic.Cacholic;
 import de.jued.dev.gc.cacholic.CacholicException;
+import de.jued.dev.gc.cacholic.db.entities.Config;
 import de.jued.dev.gc.cacholic.db.entities.Version;
+import java.io.Serializable;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.persistence.EntityManager;
+import javax.persistence.EntityTransaction;
 import javax.persistence.TypedQuery;
 
 /**
@@ -36,6 +41,92 @@ public class FastPath
         catch (Exception ex)
         {
             throw new CacholicException("Unable to get Version Information.", ex);
+        }
+    }
+    
+    /**
+     * Get an configuration object from the database
+     * 
+     * @param key Key of the object
+     * @return The object or <code>null</code> if not found.
+     */
+    public static Config getConfig(final String key)
+    {
+        try
+        {
+            final EntityManager em = Cacholic.getInstance().getEntityManagerFactory().createEntityManager();
+            final TypedQuery<Config> tqc = em.createNamedQuery("Config.getByKey", Config.class);
+            final Config result = tqc.getSingleResult();
+            em.close();
+            return (null == result ? null : result);
+        }
+        catch (Exception ex)
+        {
+            Logger.getLogger(FastPath.class.getCanonicalName()).log(Level.WARNING, String.format("Unable to get configuration object for '%s'.", key), ex);
+            return null;
+        }
+        
+    }
+    
+    /**
+     * Get a value from the configuration
+     * 
+     * @param key The key
+     * @param defValue The value if key not found or on error
+     * @return The value 
+     */
+    public static Serializable getConfigValue(final String key, final Serializable defValue)
+    {
+        final Config c = FastPath.getConfig(key);
+        return (null == c ? defValue : c.getValue());
+    }
+    
+    /**
+     * Get a value from the configuration.
+     * 
+     * @param key The key
+     * @return The value; or <code>null</code> if not found or on error
+     */
+    public static Serializable getConfigValue(final String key)
+    {
+        return FastPath.getConfigValue(key, null);
+    }
+    
+    /**
+     * Write a value to the database
+     * 
+     * @param key The key
+     * @param value The value
+     * @return <code>true</code> if everything worked
+     */
+    public boolean setConfigValue(final String key, final Serializable value)
+    {
+        try
+        {
+            final EntityManager em = Cacholic.getInstance().getEntityManagerFactory().createEntityManager();
+            final EntityTransaction transaction = em.getTransaction();
+            transaction.begin();
+            Config c = FastPath.getConfig(key);
+            if (null == c)
+            {
+                c = new Config();
+                c.setKey(key);
+                c.setValue(value);
+                em.persist(c);
+            }
+            else
+            {
+                c.setValue(value);
+                em.merge(c);
+            }
+            transaction.commit();
+            em.close();
+            return true;
+        }
+        catch (Exception e)
+        {
+            Logger.getLogger(FastPath.class.getCanonicalName()).log(Level.SEVERE, String.format("Unable to save config data for '%s'", key), e);
+            return false;
         }
     }
     
