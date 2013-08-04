544b93ef1640ec09bb2f9cb30a3b597ad6bf1fcf
Jürgen Edelbluth <dev@jued.de>
2013-08-04 21:49:35 +0200

    Included Icons from GeoCaching.com.
    The icons are used with permission for this special project only:
    
    GROUNDSPEAK SUPPORT REQUEST #386292
    
    Hi Jürgen,
    
    Thanks for the further explanation.  Yes, you can use the icons in connection
    with the display of cache data as you describe below as long as you include
    the following statement:
    
    "Cache icons © Groundspeak, Inc. (Geocaching.com). Used with permission."
    
    Jennifer Arterburn
    Director of Legal & Business Affairs
    Groundspeak Inc.
    206.753.0212 | JenA@groundspeak.com
    www.groundspeak.com
    www.geocaching.com
    
    The exact requested wording will be used in the application's "about" dialogue.

---
+++ program/Cacholic/src/main/java/de/jued/dev/gc/cacholic/gccom/Icon.java
@@ -0,0 +1,32 @@
+package de.jued.dev.gc.cacholic.gccom;
+
+/**
+ * Icon loader for GeoCaching.com Icons.
+ * 
+ * =============================================================================
+ * GROUNDSPEAK SUPPORT REQUEST #386292
+ *
+ * Hi Jürgen, 
+ *
+ * Thanks for the further explanation.  Yes, you can use the icons in connection
+ * with the display of cache data as you describe below as long as you include
+ * the following statement: 
+ * 
+ * "Cache icons © Groundspeak, Inc. (Geocaching.com). Used with permission."
+ *
+ * Jennifer Arterburn
+ * Director of Legal & Business Affairs
+ * Groundspeak Inc.
+ * 206.753.0212 | JenA@groundspeak.com 
+ * www.groundspeak.com
+ * www.geocaching.com
+ * =============================================================================
+ * 
+ * @author Juergen Edelbluth <dev@jued.de>
+ * @version 0.1
+ * @since 0.1
+ */
+public class Icon
+{
+    
+}
